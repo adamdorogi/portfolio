fde9f7838f9691f8a36e8d3225f6751f793e0994
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2024-09-14 11:32:53 +0200

    Add rate limiter

---
+++ pages/api/emails.js
@@ -0,0 +1,40 @@
+import nodemailer from 'nodemailer';
+import { rateLimit } from 'express-rate-limit'
+
+
+const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_APP_PASSWORD,
+    },
+});
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    limit: 2
+})
+
+export default async function handler(req, res) {
+    return limiter(req, res, async () => {
+        if (req.method != 'POST') {
+            res.status(405).json({ error: "Method not allowed" });
+            return;
+        }
+
+        try {
+            const { name, email, message } = JSON.parse(req.body);
+
+            await transport.sendMail({
+                to: process.env.GMAIL_EMAIL,
+                subject: `Message from ${name} (${email})`,
+                text: message
+            })
+
+            res.status(200).json({ success: "Email sent" });
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: "Internal server error" });
+        }
+    })
+}
