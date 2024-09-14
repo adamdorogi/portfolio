678543f45fa5c2ec926aac2436ccf7e87eadaaeb
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2024-09-14 11:57:05 +0200

    Remove rate limiter

--- pages/api/emails.js
+++ pages/api/emails.js
@@ -1,5 +1,4 @@
 import nodemailer from 'nodemailer';
-import { rateLimit } from 'express-rate-limit'
 
 
 const transport = nodemailer.createTransport({
@@ -10,32 +9,24 @@ const transport = nodemailer.createTransport({
     },
 });
 
-const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    limit: 2
-})
-
 export default async function handler(req, res) {
-    console.log("IP address:", req.ip)
-    return limiter(req, res, async () => {
-        if (req.method != 'POST') {
-            res.status(405).json({ error: "Method not allowed" });
-            return;
-        }
+    if (req.method != 'POST') {
+        res.status(405).json({ error: "Method not allowed" });
+        return;
+    }
 
-        try {
-            const { name, email, message } = JSON.parse(req.body);
+    try {
+        const { name, email, message } = JSON.parse(req.body);
 
-            await transport.sendMail({
-                to: process.env.GMAIL_EMAIL,
-                subject: `Message from ${name} (${email})`,
-                text: message
-            })
+        await transport.sendMail({
+            to: process.env.GMAIL_EMAIL,
+            subject: `Message from ${name} (${email})`,
+            text: message
+        })
 
-            res.status(200).json({ success: "Email sent" });
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({ error: "Internal server error" });
-        }
-    })
+        res.status(200).json({ success: "Email sent" });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
